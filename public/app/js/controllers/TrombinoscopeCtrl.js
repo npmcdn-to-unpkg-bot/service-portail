def613b0b20156941e12f2d7efa35e8a5fd969a2
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2015-11-06 11:22:48 +0100

    removed trombinoscope tiles coloring

--- public/app/js/controllers/TrombinoscopeCtrl.js
+++ public/app/js/controllers/TrombinoscopeCtrl.js
@@ -4,12 +4,6 @@ angular.module( 'portailApp' )
     .controller( 'TrombinoscopeCtrl',
 		 [ '$scope', '$state', 'currentUser', 'COULEURS',
 		   function( $scope, $state, currentUser, COULEURS ) {
-		       var randomize_colors = function( ary, couleurs ) {
-			   _(ary).each( function( item ) {
-			       item.color = couleurs[ _.random( 0, couleurs.length - 1 ) ];
-			   } );
-		       };
-
 		       $scope.filters = {
 			   regroupements_types: { classes: true,
 						  groupes_eleves: false },
@@ -20,14 +14,12 @@ angular.module( 'portailApp' )
 
 		       currentUser.regroupements().then( function ( response ) {
 			   $scope.regroupements = response;
-			   randomize_colors( $scope.regroupements, COULEURS );
 
 			   $scope.showElevesRegroupement = function( regroupement ) {
 			       $scope.regroupement = regroupement;
 			       currentUser.eleves_regroupement( regroupement.id )
 				   .then( function( response ) {
 				       $scope.eleves = response;
-				       randomize_colors( $scope.eleves, COULEURS );
 				   } );
 			   };
 
