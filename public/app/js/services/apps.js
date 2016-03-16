3b4d4702d17d09e400536b25b650a839045bddab
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2016-03-16 15:13:08 +0100

    try to fix #128

--- public/app/js/services/apps.js
+++ public/app/js/services/apps.js
@@ -2,41 +2,42 @@
 
 angular.module( 'portailApp' )
     .factory( 'Apps',
-	      [ '$resource', 'APP_PATH',
-		function( $resource, APP_PATH ) {
-		    return $resource( APP_PATH + '/api/apps/:id',
-				      { id              : '@id',
-					application_id	: '@application_id',
-					index		: '@index',
-					type		: '@type',
-					libelle		: '@libelle',
-					description	: '@description',
-					url		: '@url',
-					active		: '@active',
-					icon		: '@icon',
-					color		: '@color' },
-				      { update: { method: 'PUT' },
-					query_default: { methode: 'GET',
-							 url: APP_PATH + '/api/apps/default/',
-							 isArray: true } } );
-		} ] );
+              [ '$resource', 'APP_PATH',
+                function( $resource, APP_PATH ) {
+                    return $resource( APP_PATH + '/api/apps/:id',
+                                      { id              : '@id',
+                                        application_id	: '@application_id',
+                                        index		: '@index',
+                                        type		: '@type',
+                                        libelle		: '@libelle',
+                                        description	: '@description',
+                                        url		: '@url',
+                                        active		: '@active',
+                                        icon		: '@icon',
+                                        color		: '@color' },
+                                      { update: { method: 'PUT' },
+                                        query_default: { methode: 'GET',
+                                                         url: APP_PATH + '/api/apps/default/',
+                                                         isArray: true } } );
+                } ] );
 
 angular.module( 'portailApp' )
     .service( 'apps',
-	      [ 'Apps',
-		function( Apps ) {
-		    var apps = null;
+              [ 'Apps',
+                function( Apps ) {
+                    var _this = this;
+                    this.apps = null;
 
-		    this.query = function( force_reload ) {
-			if ( _(apps).isNull() || force_reload ) {
-			    apps = Apps.query().$promise;
-			}
+                    this.query = function( force_reload ) {
+                        if ( _(_this.apps).isNull() || force_reload ) {
+                            _this.apps = Apps.query().$promise;
+                        }
 
-			return apps;
-		    };
+                        return _this.apps;
+                    };
 
-		    this.nullify = function() {
-			apps = null;
-		    };
-		}
-	      ] );
+                    this.nullify = function() {
+                        _this.apps = null;
+                    };
+                }
+              ] );
