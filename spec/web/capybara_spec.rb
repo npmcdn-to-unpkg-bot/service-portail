e9d1b8067c799e67fd3e3c024c573c381d795d08
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2016-06-01 11:12:43 +0200

    travis twaeks

--- spec/web/capybara_spec.rb
+++ spec/web/capybara_spec.rb
@@ -6,6 +6,7 @@ feature SinatraApp do
 
   it 'loads the page Inscription CCNs 2016-2017' do
     visit 'http://localhost:9292/portail/inscription_CCN_2016/index.html'
+    p page.html
     expect(page).to have_content 'PRÉ-INSCRIPTIONS POUR LA RENTRÉE 2016-2017 !'
   end
 end
