298d3666e73be00635e516e7837900d5065059a6
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2016-06-07 09:20:10 +0200

    indentation

--- public/app/js/controllers/IframeCtrl.js
+++ public/app/js/controllers/IframeCtrl.js
@@ -2,29 +2,29 @@
 
 angular.module( 'portailApp' )
     .controller( 'IframeCtrl',
-		 [ '$scope', '$stateParams', '$sce', '$state', 'currentUser', 'Apps',
-		   function ( $scope, $stateParams, $sce, $state, currentUser, Apps ) {
-		       currentUser.get( false ).then( function ( response ) {
-			   $scope.current_user = response;
+                 [ '$scope', '$stateParams', '$sce', '$state', 'currentUser', 'Apps',
+                   function ( $scope, $stateParams, $sce, $state, currentUser, Apps ) {
+                       currentUser.get( false ).then( function ( response ) {
+                           $scope.current_user = response;
 
-			   // Les applications de l'utilisateur
-			   Apps.query()
-			       .$promise.then( function ( response ) {
-				   // Toutes les applications en iframe
-				   var app = _( response ).findWhere( { application_id: $stateParams.app } );
+                           // Les applications de l'utilisateur
+                           Apps.query()
+                               .$promise.then( function ( response ) {
+                                   // Toutes les applications en iframe
+                                   var app = _( response ).findWhere( { application_id: $stateParams.app } );
 
-				   if ( _(app).isUndefined() ) {
-				       app = _(response).findWhere( { libelle: $stateParams.app } );
+                                   if ( _(app).isUndefined() ) {
+                                       app = _(response).findWhere( { libelle: $stateParams.app } );
 
-				       // App still undefined => app is not visible from this profil => redirect to portail
-				       if ( _(app).isUndefined() ) {
-					   $state.go( 'portail.logged' );
-				       }
-				   }
+                                       // App still undefined => app is not visible from this profil => redirect to portail
+                                       if ( _(app).isUndefined() ) {
+                                           $state.go( 'portail.logged' );
+                                       }
+                                   }
 
-				   $scope.app = { nom: app.nom,
-						  url: $sce.trustAsResourceUrl( app.url ) };
-			       } );
-		       } );
-		   }
-		 ] );
+                                   $scope.app = { nom: app.nom,
+                                                  url: $sce.trustAsResourceUrl( app.url ) };
+                               } );
+                       } );
+                   }
+                 ] );
