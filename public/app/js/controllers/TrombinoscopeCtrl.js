499d90b4e9dea6d4f7de73297ba8a34a2c90393c
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2016-06-14 14:18:58 +0200

    no actions on empty tiles

--- public/app/js/controllers/TrombinoscopeCtrl.js
+++ public/app/js/controllers/TrombinoscopeCtrl.js
@@ -2,67 +2,67 @@
 
 angular.module( 'portailApp' )
     .controller( 'TrombinoscopeCtrl',
-		 [ '$scope', '$state', 'currentUser', 'COULEURS',
-		   function( $scope, $state, currentUser, COULEURS ) {
-		       $scope.filters = {
-			   regroupements_types: { classes: true,
-						  groupes_eleves: false },
-			   text: { regroupement: '',
-				   user: '' },
-			   order_asc: true
-		       };
+                 [ '$scope', '$state', 'currentUser', 'COULEURS',
+                   function( $scope, $state, currentUser, COULEURS ) {
+                       $scope.filters = {
+                           regroupements_types: { classes: true,
+                                                  groupes_eleves: false },
+                           text: { regroupement: '',
+                                   user: '' },
+                           order_asc: true
+                       };
 
-		       $scope.actions = {
-			   regroupement: [
-			       // {
-			       //	   glyphicon: 'envelope',
-			       //	   tooltip: 'Écrire un email',
-			       //	   do: function( regroupement ) {
-			       //	       console.log( 'Gonna send a nice email to ' + regroupement.libelle );
-			       //	   }
-			       // },
-			       // {
-			       //	   glyphicon: 'calendar',
-			       //	   tooltip: 'Ouvrir le cahier de textes',
-			       //	   do: function( regroupement ) {
-			       //	       console.log( 'Gonna show the CTXT for ' + regroupement.libelle );
-			       //	   }
-			       // }
-			   ],
-			   people: [
-			       // {
-			       //	   glyphicon: 'envelope',
-			       //	   tooltip: 'Écrire un email',
-			       //	   do: function( people ) {
-			       //	       console.log( 'Gonna send a nice email to ' + people.prenom + ' ' + people.nom );
-			       //	   }
-			       // },
-			       // {
-			       //	   glyphicon: 'refresh',
-			       //	   tooltip: 'Ouvrir le suivi',
-			       //	   do: function( people ) {
-			       //	       console.log( 'Gonna show the SUIVI for ' + people.prenom + ' ' + people.nom );
-			       //	   }
-			       // }
-			   ]
-		       };
+                       $scope.actions = {
+                           regroupement: [
+                               // {
+                               //          glyphicon: 'envelope',
+                               //          tooltip: 'Écrire un email',
+                               //          do: function( regroupement ) {
+                               //              console.log( 'Gonna send a nice email to ' + regroupement.libelle );
+                               //          }
+                               // },
+                               // {
+                               //          glyphicon: 'calendar',
+                               //          tooltip: 'Ouvrir le cahier de textes',
+                               //          do: function( regroupement ) {
+                               //              console.log( 'Gonna show the CTXT for ' + regroupement.libelle );
+                               //          }
+                               // }
+                           ],
+                           people: [
+                               // {
+                               //          glyphicon: 'envelope',
+                               //          tooltip: 'Écrire un email',
+                               //          do: function( people ) {
+                               //              console.log( 'Gonna send a nice email to ' + people.prenom + ' ' + people.nom );
+                               //          }
+                               // },
+                               // {
+                               //          glyphicon: 'refresh',
+                               //          tooltip: 'Ouvrir le suivi',
+                               //          do: function( people ) {
+                               //              console.log( 'Gonna show the SUIVI for ' + people.prenom + ' ' + people.nom );
+                               //          }
+                               // }
+                           ]
+                       };
 
-		       currentUser.regroupements().then( function ( response ) {
-			   $scope.regroupements = response;
+                       currentUser.regroupements().then( function ( response ) {
+                           $scope.regroupements = response;
 
-			   $scope.showElevesRegroupement = function( regroupement ) {
-			       $scope.regroupement = regroupement;
-			       currentUser.eleves_regroupement( regroupement.id )
-				   .then( function( response ) {
-				       $scope.eleves = response;
-				   } );
-			   };
+                           $scope.showElevesRegroupement = function( regroupement ) {
+                               $scope.regroupement = regroupement;
+                               currentUser.eleves_regroupement( regroupement.id )
+                                   .then( function( response ) {
+                                       $scope.eleves = response;
+                                   } );
+                           };
 
-			   $scope.retour = function() {
-			       $scope.eleves = undefined;
-			       $scope.regroupement = undefined;
-			   };
-		       });
-		   }
-		 ]
-	       );
+                           $scope.retour = function() {
+                               $scope.eleves = undefined;
+                               $scope.regroupement = undefined;
+                           };
+                       });
+                   }
+                 ]
+               );
