b03b870b47f1a96ada35382eba025a6e5e07daee
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2016-08-31 16:29:15 +0200

    change logic of adding free link tiles

--- public/app/js/controllers/PopupAjoutAppCtrl.js
+++ public/app/js/controllers/PopupAjoutAppCtrl.js
@@ -5,32 +5,19 @@ angular.module( 'portailApp' )
                  [ '$scope', '$uibModalInstance', 'APP_PATH', 'Apps', 'current_apps',
                    function( $scope, $uibModalInstance, APP_PATH, Apps, current_apps ) {
                        $scope.prefix = APP_PATH;
-                       Apps.query_default().$promise
-                           .then( function( response ) {
-                               $scope.apps = response;
-
-                               _($scope.apps).each( function( app ) {
-                                   app.available = function() {
-                                       return !_.chain(current_apps)
-                                           .reject( function( a ) {
-                                               return a.to_delete;
-                                           } )
-                                           .pluck( 'application_id' )
-                                           .contains( app.application_id )
-                                           .value();
-                                   };
-                               } );
-                               $scope.apps.push( new Apps( { creation: true,
-                                                             present: false,
-                                                             type: 'EXTERNAL',
-                                                             libelle: '',
-                                                             description: '',
-                                                             url: 'http://',
-                                                             color: '',
-                                                             active: true } ) );
-                           } );
-
                        $scope.apps_selected = false;
+
+                       $scope.add_empty_link_tile = function() {
+                           $scope.apps.push( new Apps( { creation: true,
+                                                         present: false,
+                                                         type: 'EXTERNAL',
+                                                         libelle: '',
+                                                         description: '',
+                                                         url: 'http://',
+                                                         color: '',
+                                                         active: true,
+                                                         selected: true } ) );
+                       };
 
                        $scope.keep_app_selected = function( event, app ) {
                            app.selected = false; // opposite of what we want
@@ -50,4 +37,21 @@ angular.module( 'portailApp' )
                        $scope.cancel = function () {
                            $uibModalInstance.dismiss();
                        };
+
+                       Apps.query_default().$promise
+                           .then( function( response ) {
+                               $scope.apps = response;
+
+                               _($scope.apps).each( function( app ) {
+                                   app.available = function() {
+                                       return !_.chain(current_apps)
+                                           .reject( function( a ) {
+                                               return a.to_delete;
+                                           } )
+                                           .pluck( 'application_id' )
+                                           .contains( app.application_id )
+                                           .value();
+                                   };
+                               } );
+                           } );
                    } ] );
